155337b9f5820c05cf39f46166e5a5e381cbe016
your name <you@example.com>
2022-07-04 17:45:02 -0700

    add homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,22 @@
-const Home = () => {}
+import React from 'react';
+
+const Home = () => {
+    return (
+        <body className='body2'>
+            <section>
+                <h1 className='text'>
+                    <span>G</span>
+                    <span>R</span>
+                    <span>I</span>
+                    <span>L</span>
+                    <span>L</span>
+                    <span>E</span>
+                    <span>R</span>
+                </h1>
+                <button className='button'>Enter</button>
+            </section>
+        </body>
+    );
+}
 
 export default Home;
