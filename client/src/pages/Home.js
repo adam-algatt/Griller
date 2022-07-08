4c42a558949b3af6c32069282759984bae8b99fb
Rick Barendse <dazedeyes@yahoo.com>
2022-07-07 19:37:24 -0700

    added homepage styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,14 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div className="conatiner">
+        <div className="container" >
             <h1>Welcome to Griller</h1>
             <h2>Your headquarters for all things related to the barbecue!</h2>
-            <button>Recipes</button>
-            <button>Gear</button>
-            <button>Restaurants</button>
+            <div className='buttons'>
+                <button className="recipes">Recipes</button>
+                <button className="gear">Gear</button>
+                <button className="restaurants">Restaurants</button>
+            </div>
         </div>
     );
 };
